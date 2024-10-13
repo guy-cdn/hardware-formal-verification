ede89ce34755f9894a01a5a2a435283a4c61fefb
guy <guy@email>
2024-10-13 10:45:20 +0300

    Add Jasper ibex setup

---
+++ jasper/ibex/ibex_core.f
@@ -0,0 +1,24 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+../../rtl/ibex_pkg.sv
+../../rtl/ibex_alu.sv
+../../rtl/ibex_compressed_decoder.sv
+../../rtl/ibex_controller.sv
+../../rtl/ibex_counter.sv
+../../rtl/ibex_cs_registers.sv
+../../rtl/ibex_decoder.sv
+../../rtl/ibex_ex_block.sv
+../../rtl/ibex_id_stage.sv
+../../rtl/ibex_if_stage.sv
+../../rtl/ibex_load_store_unit.sv
+../../rtl/ibex_multdiv_slow.sv
+../../rtl/ibex_multdiv_fast.sv
+../../rtl/ibex_prefetch_buffer.sv
+../../rtl/ibex_fetch_fifo.sv
+../../rtl/ibex_register_file_ff.sv
+../../rtl/ibex_core.sv
+../../rtl/ibex_csr.sv
+../../rtl/ibex_wb_stage.sv
+../../rtl/ibex_icache.sv
---
+++ jasper/ibex/setup.tcl
@@ -0,0 +1,10 @@
+###########################################
+# Setup
+###########################################
+analyze -sv09 -f ibex_core.f \
+        -incdir ../../vendor/lowrisc_ip/ip/prim/rtl/ \
+        -incdir ../../vendor/lowrisc_ip/dv/sv/dv_utils/ \
+        +define+FORMAL +define+RISCV_FORMAL +define+RVFI
+elaborate -top ibex_core
+clock clk_i
+reset ~rst_ni
