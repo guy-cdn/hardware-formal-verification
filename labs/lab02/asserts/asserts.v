76d815f5d54e711c806d8ab0637f05223e931d6f
guy <guy@email>
2024-11-18 10:57:17 +0200

    Update asserts.v

--- labs/lab02/asserts/asserts.v
+++ labs/lab02/asserts/asserts.v
@@ -1,14 +1,15 @@
-module toy(clk, ok, en, gnt, req);
+module toy(clk, ok, en, gnt, req, A, B);
 
-input clk, ok, en, gnt, req;
+input clk, ok, en, gnt, req, A, B;
 
-Q1: assert property (@(posedge clk) ok);
-Q2: assert property (@(posedge clk) clk);
-Q3: assert property (@(posedge clk) !(gnt && !req));
-Q4: assert property (@(posedge clk) en |-> ok);
-Q5: assert property (@(posedge clk) en |=> ok);
-Q6: assert property (@(posedge clk) en |=> $past(ok));
-Q7: assert property (@(posedge clk) en |=> $stable(ok));
-Q8: assert property (@(posedge clk) en |=> s_eventually(ok));
+P1: assert property (@(posedge clk) ok);
+P2: assert property (@(posedge clk) en |-> ok);
+P3: assert property (@(posedge clk) en |=> ok);
+P4: assert property (@(posedge clk) en |=> $past(ok));
+P5: assert property (@(posedge clk) en |=> $stable(ok));
+P6: assert property (@(posedge clk) en |=> s_eventually(ok));
+P7: assert property (@(posedge clk) A ##2 B);
+P8: assert property (@(posedge clk) A ##[1:3] B);
+P9: assert property (@(posedge clk) A[*4]);
 
 endmodule
