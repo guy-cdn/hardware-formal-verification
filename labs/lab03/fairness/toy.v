b11b17c666cd5eca931852979f6481b3a5ebdaed
guy <guy@email>
2024-11-24 11:07:10 +0200

    Update file

--- labs/lab03/fairness/toy.v
+++ labs/lab03/fairness/toy.v
@@ -17,6 +17,6 @@ end
 ast_liveness: assert property (@(posedge clk) s_eventually(state == 3'b000));
 
 // Assume
-fairness:     assert property (@(posedge clk) s_eventually(en));
+fairness:     assume property (@(posedge clk) s_eventually(en));
 
 endmodule
