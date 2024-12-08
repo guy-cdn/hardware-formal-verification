55bdfa3060c91def477e09533855094666be6b83
guy <guy@email>
2024-12-08 13:03:35 +0200

    Update

--- labs/lab05/register_check/check_all.sv
+++ labs/lab05/register_check/check_all.sv
@@ -18,32 +18,35 @@ module register_prop(clk, rst,
   input reg[4:0]  rvfi_rs2_addr;
   input reg[31:0] rvfi_rs2_rdata;
 
-  reg[31:0] data; // Last data written to x[rindex]
-  reg written;    // Indicator whether data was written to x[rindex]
-  reg consistent; // Indicator for consistency of write/read to x[rindex]
-  reg[4:0] rindex;// Nondeterministic choice for the register index
+  reg[31:0] data; // Last data written to x_index
+  reg written;    // Indicator whether data was written to x_index
+  reg consistent; // Indicator for consistency of write/read to x_index
+  reg[4:0] index; // Nondeterministic choice for the register index
 
   always @(posedge clk) begin
       if (!rst) begin 
           // Initial values
           written <= 1'b0;
           consistent <= 1'b1;
-      end else if (rvfi_valid && rvfi_rd_addr == rindex) begin
-          // Data is written to x[rindex]
-          data <= rvfi_rd_wdata;
-          written <= 1'b1;
-      end else if (rvfi_valid && written && rvfi_rs1_addr == rindex) begin
-          // Data is read from x[rindex]
-          consistent <= data == rvfi_rs1_rdata;
-      end else if (rvfi_valid && written && rvfi_rs2_addr == rindex) begin
-          // Data is read from x[rindex]
-          consistent <= data == rvfi_rs2_rdata;
+      end else if (rvfi_valid) begin
+          if (rvfi_rd_addr == index) begin
+              // Data is written to x_index, store written value in data
+              data <= rvfi_rd_wdata;
+              written <= 1'b1;
+          end
+          if (written && rvfi_rs1_addr == index) begin
+              // Data is read from x_index, check consistency with stored data
+              consistent <= data == rvfi_rs1_rdata;
+          end else if (written && rvfi_rs2_addr == index) begin
+              // Data is read from x_index, check consistency with stored data
+              consistent <= data == rvfi_rs2_rdata;
+          end
       end
   end
 
   consistent_x: assert property (@(posedge clk) consistent);
-  stable_rindex: assume property (@(posedge clk) 1 ##1 $stable(rindex));
-  positive_rindex: assume property (@(posedge clk) rindex != 5'd0);
+  stable_index: assume property (@(posedge clk) 1 ##1 $stable(index));
+  positive_index: assume property (@(posedge clk) index != 5'd0);
 
 endmodule
 
