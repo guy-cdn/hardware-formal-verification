5d2e0272abe16c22026a4dcfdf20eb82b4b3e4c2
guy <guy@email>
2024-12-15 09:43:57 +0200

    Update

---
+++ labs/lab06/solvers/run.tcl
@@ -0,0 +1,23 @@
+# Compile
+analyze -sv09 bmc.v
+
+# Elaborate, but don't blackbox multipliers where the sum of input bits is <= 128:
+elaborate -bbox_mul 128
+
+# Define reset and clock
+reset ~rst
+clock clk
+
+# Prove all properties (there is only one property)
+# Use engine B, which is a single property engine
+# Use increased verbosity so as to see some extra information about the run
+# Disable proof orchestration, to make sure only engine B runs
+# The property has a cover trace of length 8, but reaching Trace Attempt 8 is hard
+prove -all -engine B -verbosity 11 -orch off
+
+# Clear result, so the next proof starts fresh
+clear -result
+
+# Repeat the proof, but with a different SAT solver
+set_engine_solver solverX
+prove -all -engine B -verbosity 11 -orch off
