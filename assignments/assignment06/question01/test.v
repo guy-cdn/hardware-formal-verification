c884ca5fcc9e55f9fa7ff60f794084475e2db146
guy <guy@email>
2025-01-06 10:06:35 +0200

    Update

--- assignments/assignment06/question01/test.v
+++ assignments/assignment06/question01/test.v
@@ -1,12 +1,12 @@
-module test(clk, rst, en, state);
+module test(clk, rst, state);
 
-input clk, rst, en;
+input clk, rst;
 output reg[2:0] state;
 
 always @(posedge clk) begin
   if (~rst) begin
     state <= 3'b000;
-  end else if (en) begin
+  end else begin
     state[0] <= state == 3'b000;
     state[1] <= state[0];
     state[2] <= state[1];
